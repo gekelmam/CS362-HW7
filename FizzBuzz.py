1e3a513589943dd2f46f72b83303b2393bebda9f
Mark Gekelman <gekelmam@oregonstate.edu>
2021-03-07 23:33:26 -0800

    Update FizzBuzz.py
    This code now passes the first test case.

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -3,6 +3,10 @@ def FizzBuzzfunc(top):
 	for x in range(1, top):
 		if x % 3 == 0 and x % 5 == 0:
 			my_str += "FizzBuzz"
+		elif x % 3 == 0:
+			my_str += "Fizz"
+		elif x % 5 == 0:
+			my_str += "Buzz"
 		else:
 			my_str += str(x)
 	return my_str
