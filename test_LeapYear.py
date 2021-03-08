69a2984cb893356d435228a1b697c91bfbf96f8d
Mark Gekelman <gekelmam@oregonstate.edu>
2021-03-07 23:43:26 -0800

    Update test_LeapYear.py
    new test 2 for leap year testing year 400

--- test_LeapYear.py
+++ test_LeapYear.py
@@ -5,6 +5,7 @@ class test_LeapYear(unittest.TestCase):
     
     def test_LeapYear(self):
         self.assertEqual((LeapYear.LeapYearfunc(4)), "4 Is a leap year.")
+        self.assertEqual((LeapYear.LeapYearfunc(400)), "400 Is a leap year.")
 
 if __name__ == '__main__':
     unittest.main()
