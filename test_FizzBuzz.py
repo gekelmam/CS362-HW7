519e60ca8e73e4f0bc2f908a45f0a35f15534ebf
Mark Gekelman <gekelmam@oregonstate.edu>
2021-03-07 23:36:02 -0800

    Update test_FizzBuzz.py
    Second test case for fizzBuzz

--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -5,7 +5,7 @@ class test_FizzBuzz(unittest.TestCase):
     
     def test_FizzBuzz(self):
         self.assertEqual((FizzBuzz.FizzBuzzfunc(7)), "12Fizz4BuzzFizz" )
-            
+        self.assertEqual((FizzBuzz.FizzBuzzfunc(2)), "1" )
 
 if __name__ == '__main__':
     unittest.main()
